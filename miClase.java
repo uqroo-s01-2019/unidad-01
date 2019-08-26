e1b52d2975b9284b01b800e1487202d0c190d072
Profa Meli <39175944+mblanqueto@users.noreply.github.com>
2019-08-25 21:41:10 -0500

    Template de una clase en Java
    Detalles de una plantilla para un clase en Java con encabezado y comentarios

---
+++ miClase.java
@@ -0,0 +1,24 @@
+/*
+*  Nombre Apellidos
+* 
+*  Licencia
+*  
+*  Descripción: 
+*
+*/
+
+/* paquete */
+
+/* imports */
+
+/**
+ * Clase  -escriba la descripción de la clase-
+ * @version   1.0.0  DD MMM YYYY  -ej. 26 Ago 2019-
+ * @author    Nombre Apellido
+ */
+ 
+ public class miClase {
+ 
+  // Declarar el método main e imprimir su nombre completo y matrícula
+    
+ }
